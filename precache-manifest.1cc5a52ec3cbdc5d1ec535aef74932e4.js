b686c42cb6ddd243721b7a0aee5d6b56f4241c62
Erik Espinoza <erik_espinoza_mendoza@hotmail.com>
2021-11-08 05:51:01 -0500

    Updates

---
+++ precache-manifest.1cc5a52ec3cbdc5d1ec535aef74932e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "37328f3820540c83f416",
+    "url": "/My-First-Dapp/static/css/main.4694ad0f.chunk.css"
+  },
+  {
+    "revision": "37328f3820540c83f416",
+    "url": "/My-First-Dapp/static/js/main.37328f38.chunk.js"
+  },
+  {
+    "revision": "3e78b9bbfef140a20304",
+    "url": "/My-First-Dapp/static/css/1.090af775.chunk.css"
+  },
+  {
+    "revision": "3e78b9bbfef140a20304",
+    "url": "/My-First-Dapp/static/js/1.3e78b9bb.chunk.js"
+  },
+  {
+    "revision": "18b1c7cccbf941683da2",
+    "url": "/My-First-Dapp/static/js/runtime~main.18b1c7cc.js"
+  },
+  {
+    "revision": "d6cbd8e6d17d679dd3cd7fe3884ac8bb",
+    "url": "/My-First-Dapp/static/media/photo.d6cbd8e6.png"
+  },
+  {
+    "revision": "b9546cd0c7675bdf5b194e255bc80013",
+    "url": "/My-First-Dapp/index.html"
+  }
+];
